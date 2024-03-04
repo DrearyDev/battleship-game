e1c73490de99426b2cb05af8e4e0fd054e6a3d02
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-03-04 16:15:37 -0500

    Write createShip module to pass tests
    a factory function to create a ship with a length
    passed in as an argument.

---
+++ src/createShip.js
@@ -0,0 +1,14 @@
+
+function createShip(len) {
+    let timesHit = 0;
+
+    const length = () => { return len };
+
+    const hit = () => { return ++timesHit };
+
+    const isSunk = () => { return (length() - timesHit === 0) };
+
+    return { length, hit, isSunk };
+};
+
+export { createShip };
