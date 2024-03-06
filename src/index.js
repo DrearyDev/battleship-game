4efb0cdacebdebbf54fb88cc300f5f51ebcdbf1c
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-03-06 15:15:48 -0500

    Connect buttons and DOM functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 'use strict';
-
+/*---------------------------
+Utility styles => style.css
+---------------------------*/
 import './style.css';
+import './home.css';
 
+import { createShip } from './createShip.js';
+import { gameboard } from './gameboard.js';
+import { player } from './player.js';
+import { singlePlayer, doublePlayer } from './handleDOM.js';
+
+const singleBtn = document.querySelector('#single');
+const doubleBtn = document.querySelector('#double');
+
+singleBtn.addEventListener('click', singlePlayer);
+doubleBtn.addEventListener('click', doublePlayer);
