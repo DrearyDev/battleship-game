3bbf4cff22d021b4f39cde49efb897b74398c49a
Dreary Dev <143649090+DrearyDev@users.noreply.github.com>
2024-03-11 01:58:43 -0400

    Write function to createGrid
    will be called after user chooses single or double player

--- src/handleDOM.js
+++ src/handleDOM.js
@@ -2,9 +2,30 @@
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 
+function createGrid() {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            const div = document.createElement('div');
+            div.classList.add('square');
+
+            div.row = i;
+            div.column = j;
+            div.coord = [i,j];
+
+            grid.appendChild(div);
+        };
+    };
+    
+    body.appendChild(grid);
+};
+
 function singlePlayer() {
     body.innerHTML = '';
     body.appendChild(header);
+    createGrid();
 };
 
 function doublePlayer() { // will work on this later
