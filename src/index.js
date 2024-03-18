c1e04ed5ef9d0dd8a8e9445bdfc22d0ccdb3c483
Dreary Dev <143649090+DrearyDev@users.noreply.github.com>
2024-03-18 12:52:11 -0400

    Import setupBoardPhase
    and call it when player presses 'single player'

--- src/index.js
+++ src/index.js
@@ -10,9 +10,13 @@ import { createShip } from './createShip.js';
 import { gameboard } from './gameboard.js';
 import { player } from './player.js';
 import { singlePlayer, doublePlayer } from './handleDOM.js';
+import { setupBoardPhase } from "./setupBoardPhase.js";
 
 const singleBtn = document.querySelector('#single');
 const doubleBtn = document.querySelector('#double');
 
-singleBtn.addEventListener('click', singlePlayer);
+singleBtn.addEventListener('click', () => {
+  singlePlayer();
+  setupBoardPhase();
+});
 doubleBtn.addEventListener('click', doublePlayer);
