2e6fa4d43f90b5b8e7078837bb79f131ee5f0db2
Dreary Dev <143649090+DrearyDev@users.noreply.github.com>
2024-03-20 18:00:19 -0400

    Write functionallity for placeRandomlyBtn
    so player can place ships randomly if they want

--- src/setupBoardPhase.js
+++ src/setupBoardPhase.js
@@ -119,8 +119,6 @@ function setupBoardPhase() {
         };
       };
     });
-
-    console.log(grid.ships);
   };
   grid.addEventListener('dragend', calculateGridSet);
 
@@ -128,20 +126,104 @@ function setupBoardPhase() {
     console.log('submit');
   });
 
-  resetBtn.addEventListener('click', () => {
-    console.log('reset');
+  function resetBoard() {
     const shipsContainer = document.querySelector('.ships');
     const ships = [...grid.children].filter(ship => ship.classList.contains('ship'));
     grid.ships = new Set();
     
     [...ships].forEach(ship => {
+      const height = ship.style.height.slice(0,-2);
+      const width = ship.style.width.slice(0,-2);
+
+      if (+height > +width) {
+        ship.style.height = `${width}px`;
+        ship.style.width = `${height}px`;
+      };
+
       ship.coords = [];
       shipsContainer.appendChild(ship);
-    });
-  });
+    });    
+  };
+  resetBtn.addEventListener('click', resetBoard);
+
+  function genCoords(squaresTakenUp) {//generates coords for place-randomly btn
+    const row = Math.floor(Math.random() * 11);
+    const column = Math.floor(Math.random() * 11);
+
+    const options = {
+      up: [],
+      right: [],
+      down: [],
+      left: []
+    };
+
+    let selected = null;
+
+    for (let i = 0; i < squaresTakenUp; i++) {
+      options.up.push([row-i, column]);
+      options.right.push([row, column+i]);
+      options.down.push([row+i, column]);
+      options.left.push([row, column-i]);
+    };
+
+    for (const option in options) {
+
+      for (const coord of options[option]) {
+        if (coord[0] > 10 || coord[0] < 1 ||
+          coord[1] > 10 || coord[1] < 1) {
+          options[option] = [];
+        };
+
+        if (grid.ships.has(coord.toString())) {
+          options[option] = [];
+        };
+      };
+
+      if (options[option].length === squaresTakenUp) {
+        selected = {[option]: options[option]};
+      };
+    };
+
+    if (selected) { return selected }
+    else { return genCoords(squaresTakenUp) };
+  };
 
   placeRandomlyBtn.addEventListener('click', () => {
-    console.log('randomly place');
+    resetBoard();
+    const shipsContainer = document.querySelector('.ships');
+    const square = document.querySelector('.square');
+    const squareWidth = getComputedStyle(square).getPropertyValue('--SQUARE_WIDTH').slice(0,-2);
+
+    [...shipsContainer.children].forEach(ship => {
+      const squaresTakenUp = ship.style.width.slice(0,-2) / squareWidth;
+      const coords = genCoords(squaresTakenUp);
+      const direction = Object.keys(coords)[0];
+
+      ship.coords = coords[direction];
+
+      if (direction === 'down' || direction === 'right') {
+        ship.style.gridRow = coords[direction][0][0];
+        ship.style.gridColumn = coords[direction][0][1];
+      } else {
+        ship.style.gridRow = coords[direction][squaresTakenUp-1][0];
+        ship.style.gridColumn = coords[direction][squaresTakenUp-1][1];
+      };
+
+      const height = ship.style.height.slice(0,-2);
+      const width = ship.style.width.slice(0,-2);
+
+      if (direction === 'up' || direction === 'down') {
+        ship.style.width = `${height}px`;
+        ship.style.height = `${width}px`;
+      };
+
+      grid.appendChild(ship);
+
+      for (let i = 0; i < coords[direction].length; i++) {
+        grid.ships.add(coords[direction][i].toString());
+      };
+
+    });
   });
 };
 
