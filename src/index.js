864d91cace5fafac5710ca6480b93222ba84ee42
Dreary Dev <143649090+DrearyDev@users.noreply.github.com>
2024-04-04 01:14:40 -0400

    Write gameloop for single player
    single player mode game loop. winner is
    logged to console for now.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './game.css';
 import { createShip } from './createShip.js';
 import { gameboard } from './gameboard.js';
 import { player } from './player.js';
-import { setupShips, doublePlayer, attackPhase } from './handleDOM.js';
+import { setupShips, doublePlayer, attackPhase, updateMiniGrid } from './handleDOM.js';
 import { resetBoard } from './resetBtn.js';
 import { placeRandomly } from './placeRandomlyBtn.js';
 import { submitCoords } from './submitBtn.js';
@@ -18,8 +18,57 @@ import { setupBoardPhase } from "./setupBoardPhase.js";
 const singleBtn = document.querySelector('#single');
 const doubleBtn = document.querySelector('#double');
 
-function startGameLoop(playerOne, playerTwo) {
+function playerAttack(e, defendingPlayer) {
+  const grid = document.querySelector('.grid');
+  const div = document.createElement('div');
+  const rect = e.target.getBoundingClientRect();
+
+  const row = Math.ceil((e.clientY - rect.top) / 50);
+  const column = Math.ceil((e.clientX - rect.left) / 50);
+
+  div.style.gridRow = row;
+  div.style.gridColumn = column;
+
+  if (defendingPlayer.playerBoard.recieveAttack(row-1, column-1)) {
+    div.classList.add('hit');
+  } else {
+    div.classList.add('miss');
+  };
+
+  grid.appendChild(div);
+};
+
+function cpuAttack(player) {
+  player.playerBoard.recieveRandomAttack();
+  updateMiniGrid(player.playerBoard);
+};
+
+function checkForWinners(playerOne, playerTwo) {
+  let winner = null;
+
+  if (playerTwo.playerBoard.allShipsSunk()) {
+    winner = playerOne;
+  } else if (playerOne.playerBoard.allShipsSunk()) {
+    winner = playerTwo;
+  };
+
+  if (winner) {
+    console.log(winner.name);
+  };
+};
+
+function singleGameLoop(playerOne, playerTwo) {
   attackPhase(playerOne);
+
+  const grid = document.querySelector('.grid');
+
+  grid.addEventListener('click', (e) => {
+    if (e.target === grid) {
+      playerAttack(e, playerTwo);
+      cpuAttack(playerOne);
+      checkForWinners(playerOne, playerTwo);
+    };
+  });
 };
 
 singleBtn.addEventListener('click', () => {
@@ -30,17 +79,17 @@ singleBtn.addEventListener('click', () => {
   const placeRandomlyBtn = document.querySelector('.place-randomly');
   const submitBtn = document.querySelector('.submit');
 
-  const winner = null;
   const playerOne = player();
+  playerOne.name = 'player one';
   const cpu = player();
-  cpu.playerBoard.placeRandomShips();
-
+  cpu.name = 'computer';
 
   resetBtn.addEventListener('click', resetBoard);
   placeRandomlyBtn.addEventListener('click', placeRandomly);
   submitBtn.addEventListener('click', () => {
     submitCoords(playerOne);
-    startGameLoop(playerOne, cpu);
+    cpu.playerBoard.placeRandomShips();
+    singleGameLoop(playerOne, cpu);
   });
 
 });
